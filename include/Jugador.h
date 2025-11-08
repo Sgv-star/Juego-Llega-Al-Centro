b47d38a99ce1d379da45e3f3a5d7b57375724737
Sgv-star <sgonzalezvillada@gmail.com>
2025-11-08 16:17:44 -0500

    Se construye la clase jugador

--- include/Jugador.h
+++ include/Jugador.h
@@ -1,15 +1,38 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
+#include <string>
 
+using namespace std;
 
 class Jugador
 {
     public:
         Jugador();
+        Jugador(string nombre, int PVInicial, int PVActual, int numeroJugador, bool estado, int posicionActual);
+
+        void setNombre(string nombre);
+        void setPVInicial(int PVInicial);
+        void setPVActual(int PVActual);
+        void setNumeroJugador(int numeroJugador);
+        void setEstado(bool estado);
+        void setPosicionActual(int posicionActual);
+
+        string getNombre();
+        int getPVInicial();
+        int getPVActual();
+        int getNumeroJugador();
+        bool getEstado();
+        int getPosicionActual();
 
     protected:
 
     private:
+        string nombre;
+        int PVInicial;
+        int PVActual;
+        int numeroJugador;
+        bool estado;
+        int posicionActual;
 };
 
 #endif // JUGADOR_H
--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -4,3 +4,60 @@ Jugador::Jugador()
 {
     //ctor
 }
+
+Jugador::Jugador(string nombre, int PVInicial, int PVActual, int numeroJugador, bool estado, int posicionActual){
+    this -> nombre = nombre;
+    this -> PVInicial = PVInicial;
+    this -> PVActual = PVActual;
+    this -> numeroJugador = numeroJugador;
+    this -> estado = estado;
+    this -> posicionActual = posicionActual;
+}
+
+void Jugador::setNombre(string nombre){
+    this -> nombre = nombre;
+}
+
+void Jugador::setPVInicial(int PVInicial){
+    this -> PVInicial = PVInicial;
+}
+
+void Jugador::setPVActual(int PVActual){
+    this -> PVActual = PVActual;
+}
+
+void Jugador::setNumeroJugador(int numeroJugador){
+    this -> numeroJugador = numeroJugador;
+}
+
+void Jugador::setEstado(bool estado){
+    this -> estado = estado;
+}
+
+void Jugador::setPosicionActual(int posicionActual){
+    this -> posicionActual = posicionActual;
+}
+
+string Jugador::getNombre(){
+    return nombre;
+}
+
+int Jugador::getPVInicial(){
+    return PVInicial;
+}
+
+int Jugador::getPVActual(){
+    return PVActual;
+}
+
+int Jugador::getNumeroJugador(){
+    return numeroJugador;
+}
+
+bool Jugador::getEstado(){
+    return estado;
+}
+
+int Jugador::getPosicionActual(){
+    return posicionActual;
+}
